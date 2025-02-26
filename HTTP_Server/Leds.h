b325b589aa29325ec9cc2b37dfeb6ff5b10e6247
oscar-ocaña-casado <oscar.ocana.casado@alumnos.upm.es>
2025-02-26 21:57:01 +0100

    Servidor Web final de la Practica 1

---
+++ HTTP_Server/Leds.h
@@ -0,0 +1,13 @@
+#ifndef __LEDS_H
+#define __LEDS_H
+
+#include "stm32f4xx_hal.h"
+
+//Cabeceras de funciones
+int32_t LED_Initialize (void);
+int32_t LED_Uninitialize (void);
+int32_t LED_On (uint32_t num);
+int32_t LED_Off (uint32_t num);
+int32_t LED_SetOut (uint32_t val);
+
+#endif /* __LEDS_H*/
